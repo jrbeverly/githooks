c3e4cf4e81405fb08ae10a1f39faf62750909370
jrbeverly <jrbeverly@live.com>
2017-09-13 21:42:03 -0400

    Adding docker-bats execution

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 DIR=$(dirname $(readlink -f "$0"))
 
+#
+# Verification
+#
+command -v bats >/dev/null 2>&1 || { echo >&2 "The script requires 'bats' but it's not installed.  Aborting."; exit 1; }
+command -v boxes >/dev/null 2>&1 || { echo >&2 "The script requires 'boxes' but it's not installed.  Aborting."; exit 1; }
+
+#
+# Main
+#
+
 echo "Running integration tests." | boxes -d dog
 
 for file in "$DIR/"*.bats; do
